cd600a94fe80b19f1ed11c16fe266c6f76917849
Sarah Hampton <stoluba@gmail.com>
2022-11-12 15:47:46 -0500

    Update button ids in html and add to game class

--- game.js
+++ game.js
@@ -5,34 +5,20 @@ class Game {
         this.currentPlayer = this.leftPlayer;
         this.turnsPlayed = 0;
         this.tie = false;
-        this.board = {
-            topLeftButton = '',
-            topCenterButton = '',
-            topRightButton = '',
-            middleLeftButton = '',
-            middleCenterButton = '',
-            middleRightButton = '',
-            bottomLeftButton = '',
-            bottomCenterButton = '',
-            bottomRightButton = ''
-        };
-        this.winPositions = [win1, win2, win3, win4, win5, win6, win7, win8];
+        this.board = [
+            0,1,2,
+            3,4,5,
+            6,7,8,
+        ];
+        this.winPositions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
     }
 
-    
     resetBoard() {
-        // clear all buttons
-        this.board = {
-            topLeftButton = '',
-            topCenterButton = '',
-            topRightButton = '',
-            middleLeftButton = '',
-            middleCenterButton = '',
-            middleRightButton = '',
-            bottomLeftButton = '',
-            bottomCenterButton = '',
-            bottomRightButton = ''
-        };
+        this.board = [
+            0,1,2,
+            3,4,5,
+            6,7,8,
+        ];
         this.tie = false;
         this.turnsPlayed = 0;
     }
@@ -47,11 +33,16 @@ class Game {
     }
 
     checkForTie() {
-        if (this.turnsPlayed === 9 && ) {
+        if (this.turnsPlayed === 9) {
             this.tie = true;
             resetBoard();
             return 'TIE GAME!';
         }
+    }
+
+    checkForWins() {
+        // if X or O is in win positions
+        // update
     }
 
 
@@ -61,31 +52,26 @@ class Game {
 }
 
 
-// Two Player instances
-
-// A way to keep track of the data for the game board
-// array or object?
-// number of positions, positions taken, positions available
-
-// A way to keep track of which player’s turn it currently is
-// every time function is invoked this.currentPlayer is updated
-// function is invoked when
-
-// A way to check the Game’s board data for win conditions
-// 
-
-// A way to detect when a game is a draw (no one has won)
-// all positions are filled, no pattern has been made to match a winning combo
-// this.tie = true or false
-// function if (board is a tie) {
-//     show message that game was a tie
-//     resetBoard()
-// }
-
-// A way to reset the Game’s board to begin a new game
-// clear all buttons resetBoard()
+// new instance of game
+// left player defaults to start game
+// player selects position-store position
+// update current game board
+// this.board[i] = X or O
+// compare current game board to winPositions-function
+// switch player-function to reassign this.currentPlayer
+// player selects position-store position
+// update current game board
+// compare current game board to winPositions-function
+// if win happens:
+// check to see if X or O has won, update players win count.
 
 
+// invoke function to switch player-update turn banner
+// click button, track click, only allow click on "empty" button
 
-
-
+// switch player-
+// click button, on 5th click check for wins
+// continue to switch players and check for wins until 9 plays have happened, tie
+// if win happens:
+// check to see if X or O has won, update players win count.
+// once win or tie happen, display message and reset the board.
